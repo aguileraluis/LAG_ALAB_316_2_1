69f00b502e1a6ee64455650acc0115ed96ab6592
Luis Aguilera Garcia <luis.kuniversity@gmail.com>
2024-10-14 17:12:48 -0700

    fixed gusses bug

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const tries = 5;
+const tries = 6;
 let random = [];
-let guesses = 0;
+let guesses = 1;
+let guessed = false; 
 
 let userConsent = confirm(
   "Welcome to 'Guessing Game' click okay below to play!"
@@ -12,38 +13,51 @@ for (let i = 0; i < 1; i++) {
 
 let computerNumber = random[0];
 
-console.log("Im the computer number" + computerNumber);
+if (userConsent && guesses !== tries) {
 
-if (userConsent) {
+
   function getNumber() {
-    let userGuessed = Number(prompt("Please enter a number from 1 to 12."));
 
-    if (userGuessed) {
-      checkGuess(userGuessed);
+    if (guesses !== tries) {
+      let userGuessed = Number(window.prompt("Please enter a number from 1 to 12."));
+
+      if (userGuessed) {
+        checkGuess(userGuessed);
+      } else {
+        window.alert("Ok! May be some other time.");
+        return;
+      }
     } else {
-      alert("Ok! May be some other time.");
-      return;
-    }
-  }
+      window.alert(
+        `Sorry! only ${tries - 1} guesses allowed. The number was ${random[0]} Thank you for playing!`
+      );
 
-  getNumber();
+      let restart = confirm("Do you want to play again?");
+      if (restart) {
+        location.reload();
+      } else {
+        window.alert("Ok! May be some other time.");
+      }
+    }
+    
+  }
 
   function checkGuess(guessItem) {
     if (guesses < tries) {
       if (guessItem == computerNumber) {
         changeGuessColor(guessItem, "correct");
       } else if (guessItem > computerNumber && guessItem !== null) {
+        guesses += 1; 
         changeGuessColor(guessItem, "lower");
-        guesses += 1;
       } else if (guessItem < computerNumber && guessItem !== null) {
+        guesses += 1; 
         changeGuessColor(guessItem, "higher");
-        guesses += 1;
       } else {
-        alert("Ok! May be some other time.");
+        window.alert("Ok! May be some other time.");
         return;
       }
     } else if (guesses === tries) {
-      alert(
+      window.alert(
         `Sorry! only ${tries} guesses allowed. The number was ${random[0]} Thank you for playing!`
       );
 
@@ -51,139 +65,178 @@ if (userConsent) {
       if (restart) {
         location.reload();
       } else {
-        alert("Ok! May be some other time.");
+        window.alert("Ok! May be some other time.");
       }
     }
   }
 
   function changeGuessColor(guess, guessPosition) {
-    console.log(
-      "Im the guess inside the changeguesscolor function " +
-        guessPosition +
-        guess
-    );
     if (guess == 1) {
       if (guessPosition == "correct") {
         document.getElementById("item1").style.backgroundColor = "blue";
+        setTimeout(() => {
+          alert(`Congratulations! You win. The number is ${computerNumber}`);
+        }, 400);
         return;
       } else {
         document.getElementById("item1").style.backgroundColor = "grey";
 
-        alert(`Sorry! ${guess} is the wrong number. Guess ${guessPosition}.`);
+        alert(`Sorry! ${guess} is the wrong number. Guess ${guessPosition}. You have ${tries - guesses} guesses left`);
       }
       setTimeout(getNumber, 400);
     } else if (guess == 2) {
       if (guessPosition == "correct") {
         document.getElementById("item2").style.backgroundColor = "blue";
+        setTimeout(() => {
+          alert(`Congratulations! You win. The number is ${computerNumber}`);
+        }, 400); 
         return;
       } else {
         document.getElementById("item2").style.backgroundColor = "grey";
 
-        alert(`Sorry! ${guess} is the wrong number. Guess ${guessPosition}.`);
+        alert(`Sorry! ${guess} is the wrong number. Guess ${guessPosition}. You have ${tries - guesses} guesses left`);
       }
       setTimeout(getNumber, 400);
     } else if (guess == 3) {
       if (guessPosition == "correct") {
         document.getElementById("item3").style.backgroundColor = "blue";
+        setTimeout(() => {
+          alert(`Congratulations! You win. The number is ${computerNumber}`);
+        }, 400);
+     
         return;
       } else {
         document.getElementById("item3").style.backgroundColor = "grey";
 
-        alert(`Sorry! ${guess} is the wrong number. Guess ${guessPosition}.`);
+        alert(`Sorry! ${guess} is the wrong number. Guess ${guessPosition}. You have ${tries - guesses} guesses left`);
       }
       setTimeout(getNumber, 400);
     } else if (guess == 4) {
       if (guessPosition == "correct") {
         document.getElementById("item4").style.backgroundColor = "blue";
+        setTimeout(() => {
+          alert(`Congratulations! You win. The number is ${computerNumber}`);
+        }, 400);
         return;
       } else {
         document.getElementById("item4").style.backgroundColor = "grey";
 
-        alert(`Sorry! ${guess} is the wrong number. Guess ${guessPosition}.`);
+        alert(`Sorry! ${guess} is the wrong number. Guess ${guessPosition}. You have ${tries - guesses} guesses left`);
       }
       setTimeout(getNumber, 400);
     } else if (guess == 5) {
       if (guessPosition == "correct") {
         document.getElementById("item5").style.backgroundColor = "blue";
+        
+        setTimeout(() => {
+          alert(`Congratulations! You win. The number is ${computerNumber}`);
+        }, 400);
+        
         return;
       } else {
         document.getElementById("item5").style.backgroundColor = "grey";
 
-        alert(`Sorry! ${guess} is the wrong number. Guess ${guessPosition}.`);
+        alert(`Sorry! ${guess} is the wrong number. Guess ${guessPosition}. You have ${tries - guesses} guesses left`);
       }
       setTimeout(getNumber, 400);
     } else if (guess == 6) {
       if (guessPosition == "correct") {
         document.getElementById("item6").style.backgroundColor = "blue";
-        return;
+        setTimeout(() => {
+          alert(`Congratulations! You win. The number is ${computerNumber}`);
+        }, 400);
+        return; 
       } else {
         document.getElementById("item6").style.backgroundColor = "grey";
 
-        alert(`Sorry! ${guess} is the wrong number. Guess ${guessPosition}.`);
+        alert(`Sorry! ${guess} is the wrong number. Guess ${guessPosition}. You have ${tries - guesses} guesses left`);
       }
       setTimeout(getNumber, 400);
     } else if (guess == 7) {
       if (guessPosition == "correct") {
         document.getElementById("item7").style.backgroundColor = "blue";
-        return;
+        setTimeout(() => {
+          alert(`Congratulations! You win. The number is ${computerNumber}`);
+        }, 400);
+        return; 
       } else {
         document.getElementById("item7").style.backgroundColor = "grey";
 
-        alert(`Sorry! ${guess} is the wrong number. Guess ${guessPosition}.`);
+        alert(`Sorry! ${guess} is the wrong number. Guess ${guessPosition}. You have ${tries - guesses} guesses left`);
       }
       setTimeout(getNumber, 400);
     } else if (guess == 8) {
       if (guessPosition == "correct") {
         document.getElementById("item8").style.backgroundColor = "blue";
+        setTimeout(() => {
+          alert(`Congratulations! You win. The number is ${computerNumber}`);
+        }, 400); 
         return;
       } else {
         document.getElementById("item8").style.backgroundColor = "grey";
 
-        alert(`Sorry! ${guess} is the wrong number. Guess ${guessPosition}.`);
+        alert(`Sorry! ${guess} is the wrong number. Guess ${guessPosition}. You have ${tries - guesses} guesses left`);
       }
       setTimeout(getNumber, 400);
     } else if (guess == 9) {
       if (guessPosition == "correct") {
         document.getElementById("item9").style.backgroundColor = "blue";
-        return;
+        setTimeout(() => {
+          alert(`Congratulations! You win. The number is ${computerNumber}`);
+        }, 400);
+        return; 
       } else {
         document.getElementById("item9").style.backgroundColor = "grey";
 
-        alert(`Sorry! ${guess} is the wrong number. Guess ${guessPosition}.`);
+        alert(`Sorry! ${guess} is the wrong number. Guess ${guessPosition}. You have ${tries - guesses} guesses left`);
       }
       setTimeout(getNumber, 400);
     } else if (guess == 10) {
       if (guessPosition == "correct") {
         document.getElementById("item10").style.backgroundColor = "blue";
+        setTimeout(() => {
+          alert(`Congratulations! You win. The number is ${computerNumber}`);
+        }, 400); 
         return;
       } else {
         document.getElementById("item10").style.backgroundColor = "grey";
 
-        alert(`Sorry! ${guess} is the wrong number. Guess ${guessPosition}.`);
+        alert(`Sorry! ${guess} is the wrong number. Guess ${guessPosition}. You have ${tries - guesses} guesses left`);
       }
       setTimeout(getNumber, 400);
     } else if (guess == 11) {
       if (guessPosition == "correct") {
         document.getElementById("item11").style.backgroundColor = "blue";
-        return;
+        setTimeout(() => {
+          alert(`Congratulations! You win. The number is ${computerNumber}`);
+        }, 400);
+        return; 
       } else {
         document.getElementById("item11").style.backgroundColor = "grey";
 
-        alert(`Sorry! ${guess} is the wrong number. Guess ${guessPosition}.`);
+        alert(`Sorry! ${guess} is the wrong number. Guess ${guessPosition}. You have ${tries - guesses} guesses left`);
       }
       setTimeout(getNumber, 400);
     } else if (guess == 12) {
       if (guessPosition == "correct") {
         document.getElementById("item12").style.backgroundColor = "blue";
+        setTimeout(() => {
+          alert(`Congratulations! You win. The number is ${computerNumber}`);
+        }, 400);
         return;
       } else {
         document.getElementById("item12").style.backgroundColor = "grey";
 
-        alert(`Sorry! ${guess} is the wrong number. Guess ${guessPosition}.`);
+        alert(`Sorry! ${guess} is the wrong number. Guess ${guessPosition}. You have ${tries - guesses} guesses left.`);
       }
       setTimeout(getNumber, 400);
+    } else {
+      setTimeout(getNumber, 400); 
+      guesses += 1; 
     }
   }
-} else {
-  alert("Ok! May be some other time.");
-}
+ 
+  getNumber(); 
+
+} 
+ 
